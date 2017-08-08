74bddb69c0f173131dd92707b59161e4d53c8741
John <John@PC>
2017-08-08 03:18:12 -0400

    Beginning work

---
+++ src/Constants.java
@@ -0,0 +1,9 @@
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+
+public class Constants {
+	  public static Twitter TWITTER = TwitterFactory.getSingleton();
+	  public static final int PAGE_SIZE = 20;
+
+
+}
--- src/FakeTweets.java
+++ src/FakeTweets.java
@@ -1,10 +1,61 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+import twitter4j.Paging;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.User;
 
 public class FakeTweets {
+	protected static List<String> tweets = new ArrayList<>();
+	protected static String userName;
+	protected static String tempFile;
+	private static Date createdate;
+	private static int followersCount;
+	private static int followingCount;
+
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("I Still exit");
+		Scanner scan = new Scanner(System.in);
+		
+		//The following will be used to check custom accounts
+		//System.out.println("Welcome to FakeTweets! Please enter the account you'd like to examine:");
+		//userName = scan.nextLine();
+		userName = "congressedits";
+		fetchUserTweets(userName, 100);
+		
 
 	}
+
+	private static void fetchUserTweets(String userName, int numTweets) {
+		try {
+			//Fetch basic info
+			User user = Constants.TWITTER.showUser(userName);
+			createdate = user.getCreatedAt();
+			followersCount = user.getFollowersCount();
+			followingCount = user.getFriendsCount();
+			
+			Paging page = new Paging(1, Constants.PAGE_SIZE);
+			for (int p = 1; p <= Math.ceil((double) numTweets / Constants.PAGE_SIZE); p++) {
+				page.setPage(p);
+				for (Status status : Constants.TWITTER.getUserTimeline(userName, page))
+					tweets.add(status.getText());
+			}     
+			int numberTweets = tweets.size();			
+		}
+		
+		catch (TwitterException e) {
+			System.out.println("Unable to retrieve tweets from Twitter for " + userName + " at this time.");
+			System.out.println(e.getMessage());
+		}
+	}   
 
 }
